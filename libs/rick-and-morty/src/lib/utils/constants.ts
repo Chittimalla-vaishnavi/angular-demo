5a827b8e1ab6436dad6bf207dc4a8033efafdc6e
Vaishnavi <vaishnavichittimalla99@gmail.com>
2026-01-19 16:25:00 +0530

    update chart labels color

--- libs/rick-and-morty/src/lib/utils/constants.ts
+++ libs/rick-and-morty/src/lib/utils/constants.ts
@@ -3,13 +3,11 @@ export const CHART_OPTIONS = {
     legend: {
       labels: {
         usePointStyle: true,
-        color: '#fff',
+        color: '#9d9b9b',
       },
     },
   },
 };
-
-
 
 export const lightNeutralColorForName = (name: string) =>
   `hsl(${[...name].reduce((a, c) => a + c.charCodeAt(0), 0) % 360}, 35%, 80%)`;
